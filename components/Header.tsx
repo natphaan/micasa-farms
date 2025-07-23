c988f0979054cf5f290d88ff452779acf5983789
nathanphan <nathan.phan@ucalgary.ca>
2025-07-23 14:05:58 -0600

    header outline completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
+import { ShoppingCart, User } from 'lucide-react';
+import { UserRound } from 'lucide-react'
+import { Globe } from 'lucide-react'
+
 
 const Header = () => {
   return (
     <header className='w-full h-16 bg-white border-b border-gray-200'> 
         <div className='container mx-auto h-full flex items-center justify-between'>
 
-                {/* nav links*/}
+                {/* nav links on left side of header*/}
                 <nav className='flex items-center'>
                         <ul className='flex items-center space-x-4'>
                                 <li> <a href = "#" className='text-gray-600 text-md hover:text-black'>Products</a> </li>
@@ -14,12 +19,32 @@ const Header = () => {
                         </ul>   
                 </nav>
 
-                <div className='text-black'>
-                        Micasa Farms Logo
+
+                {/* Micasa Farms logo --> just a placement for now */}
+                <div>
+                        <Image
+                                src = "/images/js.svg"
+                                alt = "Javascript logo"
+                                width = {60}
+                                height = {60} 
+                        />
                 </div>
 
+
+
+                {/* nav links on right side of header*/}
+
                 <div className='text-black'>
-                        dwdwd
+                        <nav className='flex items-center'>
+                                <ul className='flex items-center space-x-4'>
+                                        <li>  <a href = "#"> <ShoppingCart /> </a> </li>
+                                        <li> <UserRound /> </li>
+                                        <li> <Globe /> </li>
+
+
+                                </ul>
+
+                        </nav>
 
                 </div>
 
